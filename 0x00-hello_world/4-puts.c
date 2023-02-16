19d260fbf24105f711d1955e3e763d7cc9c1f871
rsa015 <sadjignon015@hotmail.com>
2023-02-16 14:10:09 -0800

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * print a string starting with ".
+ * print a string starting with \".
  *
  * puts instead of printf
  * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
