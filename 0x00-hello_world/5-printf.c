8800a78043f992173a6fecce009da0a0d68b26f0
rsa015 <sadjignon015@hotmail.com>
2023-02-16 14:22:04 -0800

    print it

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - main entry function
+ *
+ * display text using printf
+ */
+
+int main(void)
+{
+	printf("with a proper grammar, but the outcome is piece of art,\n");
+	return (0);
+}
