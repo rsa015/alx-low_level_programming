fa50894dcd7554f2c0d5eb8d9512b7c8822507e0
rsa015 <sadjignon015@hotmail.com>
2023-02-21 14:04:45 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * _islower - Checks if the params is a lower case value.
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
