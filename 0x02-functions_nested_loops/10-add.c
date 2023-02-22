ce199f347696044a1723f522a9a408d6bf0007bd
rsa015 <sadjignon015@hotmail.com>
2023-02-22 12:59:37 -0800

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integer.
+ *
+ * @a: parameter one of type integer
+ * @b: parameter two of type integer
+ *
+ * Return: The summation of integer a and b
+ */
+int add (int a, int b)
+{
+	return (a + b);
+}
