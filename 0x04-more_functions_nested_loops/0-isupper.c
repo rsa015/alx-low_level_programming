bbc717647356583ff6bdabbd95efb2823f096ebe
rsa015 <sadjignon015@hotmail.com>
2023-02-24 14:10:19 -0800

    Uppercase checker

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if parameter is an uppercase character.
+ * @c: input character
+ * Return: 1 if the input is uppercase, 0 otherwise.
+ */
+int _isupper(int ch)
+{
+	if (ch >= 65 && ch <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
