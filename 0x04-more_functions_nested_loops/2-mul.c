dd21fd95741571ef0abb200ebfb3aed2be42247d
rsa015 <sadjignon015@hotmail.com>
2023-02-24 14:35:36 -0800

    Mutiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first input number;
+ * @b: second input number;
+ * Return: mutiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
