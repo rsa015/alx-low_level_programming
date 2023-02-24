c78a17192fa492193d262ec33b5f75874941ee06
rsa015 <sadjignon015@hotmail.com>
2023-02-24 14:23:59 -0800

    digit function

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if the a given argument is a digit.
+ * @ch: inpt parameter
+ * Return: 1 to indicate success and 0 otherwise.
+ */
+int _isdigit(int ch)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
