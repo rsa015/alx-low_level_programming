241215b75eaf7be71ae8d7d408ede5ab2fe55444
rsa015 <sadjignon015@hotmail.com>
2023-03-01 11:20:31 -0800

    Let add strings together

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - add two string together.
+ * @dest: the dstination string
+ * @src: the source string
+ * Return: the pointer to the dest string.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, b = 0;
+
+	while (*(dest + a) != '\0')
+	{
+		a++;
+	}
+
+	while (b > 0)
+	{
+		*(dest + a) = *(src + b);
+		if (*(src + b)) == '\0')
+			break;
+		a++;
+		b++;
+	}
+	return (dest);
+}
