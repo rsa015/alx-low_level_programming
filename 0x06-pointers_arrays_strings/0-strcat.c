f0fb4d44bc311d7d1a9c0d3068accc5841e0c4bc
rsa015 <sadjignon015@hotmail.com>
2023-03-01 11:31:54 -0800

    Check condition

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int a = 0, b = 0;
+	int a = 0;
+	int b = 0;
 
 	while (*(dest + a) != '\0')
 	{
 		a++;
 	}
 
-	while (b > 0)
+	while (b >= 0)
 	{
 		*(dest + a) = *(src + b);
 		if (*(src + b)) == '\0')
