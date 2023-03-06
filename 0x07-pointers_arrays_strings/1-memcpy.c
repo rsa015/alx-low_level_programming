5c35ef14f52e371ac0039575c6c99c13d9ca5b94
rsa015 <sadjignon015@hotmail.com>
2023-03-06 12:09:27 -0800

    Copy memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * memcpy - copies memory area.
+ * @dest: dstination memory area.
+ * @src: source memory area
+ * @n: bytes filled.
+ * Return: the pointer to the dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,5 +2,7 @@
 #define _MAIN_H_
 
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 
 #endif
