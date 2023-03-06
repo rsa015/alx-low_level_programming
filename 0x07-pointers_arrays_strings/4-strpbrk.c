4b2af9ef9c1b492be8eac9ad422e75b72f1368bb
rsa015 <sadjignon015@hotmail.com>
2023-03-06 14:20:42 -0800

    Loacte a substring

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * _strpbrk - searches a string for any of the set of bytes.
+ * _strpbrk - locates the first occurrence in the string s of any of the bytes in the string accept.
  * @s: first string.
  * @accept: second string.
- * Return: a pointer to the bytes in s that matches one of the bytes in accept.
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * or a NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
+
+	i = 0;
+	j = 0;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
