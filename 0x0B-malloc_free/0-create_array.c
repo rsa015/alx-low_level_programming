2fd66032203b313a0b64ee6d5a8f0fe975623315
rsa015 <sadjignon015@hotmail.com>
2023-03-13 16:49:03 -0700

    Array creation

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of char.
+ * @size: size of the created array.
+ * @c: char parameter.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *cr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	cr = malloc(sizeof(c) * size);
+
+	if (cr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		cr[i] = c;
+
+	return (cr);
+}
