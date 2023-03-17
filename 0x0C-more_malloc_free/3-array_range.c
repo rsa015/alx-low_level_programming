b54b4b6c1be5d9d32bf524c2ea9f67b7cc4a216a
rsa015 <sadjignon015@hotmail.com>
2023-03-16 17:26:51 -0700

    create a range array based on min and max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - create an array of integer.
+ * @min: minimum value
+ * @max: maximum value.
+ * Return: pointer to the newly created array.
+ */
+int *array_range(int min, int max)
+{
+	int *ar;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; min++)
+		ar[i] = min;
+
+	return (ar);
+}
