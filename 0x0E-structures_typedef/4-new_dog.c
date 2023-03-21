ec654c09e3673a52907de2c6c0f27e875bf189b1
rsa015 <sadjignon015@hotmail.com>
2023-03-21 16:33:02 -0700

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,7 +26,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		;
 
 	p_dog->name = malloc(lname + 1);
-	p_dog->owner = malloc(lower + 1);
+	p_dog->owner = malloc(lowner + 1);
 
 	if (!(p_dog->name) || !(p_dog->owner))
 	{
