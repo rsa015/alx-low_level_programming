7fa6ae51ad42787f12c322d0ed487280db7c751a
rsa015 <sadjignon015@hotmail.com>
2023-03-20 17:25:15 -0700

    Structure of the dog: name, age and owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - struct that stores information of the dog obj.
+ * @name: name of the dog.
+ * @age: age in the dog.
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name,
+ * its age and its owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
+#endif
