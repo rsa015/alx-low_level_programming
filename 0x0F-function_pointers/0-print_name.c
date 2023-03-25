bd441a402c4dbaf130d06a27ea504a3f43cede92
rsa015 <sadjignon015@hotmail.com>
2023-03-25 05:47:51 -0700

    minor changes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 /**
  * print_name - prints a name.
  * @name: input name.
  * @f: function pointer.
- * Return: void
+ * Return: no return
  */
 void print_name(char *name, void (*f)(char *))
 {
