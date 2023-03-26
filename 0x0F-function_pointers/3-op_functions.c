8b5d6805e1d58a1dfbfca37276ee0f37cc5e0eb0
rsa015 <sadjignon015@hotmail.com>
2023-03-26 11:13:21 -0700

    Add description for op_add function

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
-/**op_add - add two numbers.
+/**
+ * op_add - add two numbers.
  * @a: first number.
  * @b: second number.
  * Return: add.
@@ -51,7 +52,7 @@ int op_div(int a, int b)
  * op_mod - calculates the module of two numbers.
  * @a: first number.
  * @b: second number.
- * Return: remainder  of the division.
+ * Return: remainder of the division.
  */
 int op_mod(int a, int b)
 {
