5fe765ae2401fdad61127c1099b84c1b10d490b6
rsa015 <sadjignon015@hotmail.com>
2023-03-31 16:46:38 -0700

    add header file

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: pointer to the next node.
+ *
+ * Description: singly linked list node structure.
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+
+#endif
